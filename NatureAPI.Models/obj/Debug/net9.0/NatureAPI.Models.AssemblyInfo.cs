6f5f60f325140cefe383e606405e0319e3a7a43e
Hassiel <hassiel_am02@outlook.com>
2025-11-25 17:53:41 -0600

    listo

--- NatureAPI.Models/obj/Debug/net9.0/NatureAPI.Models.AssemblyInfo.cs
+++ NatureAPI.Models/obj/Debug/net9.0/NatureAPI.Models.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NatureAPI.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c545405a6d62cb9ea9c22b0de0be955c9c0edbb3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19d2c179015e6d05d720fb4fe939dadbb8367be8")]
 [assembly: System.Reflection.AssemblyProductAttribute("NatureAPI.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NatureAPI.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
